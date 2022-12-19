8c5c4d8fb77e0bc2a061dc65fa98d123041f6d82
tmnt150 <Ootmnt150oO@gmail.com>
2022-12-19 19:46:52 +0800

    讓調用API更加合理化

---
+++ src/main/java/NWTW/Engine/NWTWEngineAPI.java
@@ -0,0 +1,44 @@
+package NWTW.Engine;
+
+import NWTW.Engine.BossBar.BossBarManager;
+import NWTW.Engine.CustomItem.CustomItemManager;
+import NWTW.Engine.CustomSkin.SkinManager;
+import NWTW.Engine.GeoIP.GeoIP;
+import NWTW.Engine.Inventory.InventoryManager;
+import NWTW.Engine.ScoreBoard.ScoreboardManager;
+import NWTW.Engine.Translate.TranslateManager;
+import com.google.gson.Gson;
+
+public class NWTWEngineAPI {
+    public static ScoreboardManager getScoreboardManager() {
+        return NWTWEngine.getPlugin().getScoreboardManager();
+    }
+
+    public static GeoIP getIpManager() {
+        return NWTWEngine.getPlugin().getIpManager();
+    }
+
+    public static InventoryManager getInventoryManager() {
+        return NWTWEngine.getPlugin().getInventoryManager();
+    }
+
+    public static BossBarManager getBossBarManager() {
+        return NWTWEngine.getPlugin().getBossBarManager();
+    }
+
+    public static SkinManager getSkinManager() {
+        return NWTWEngine.getPlugin().getSkinManager();
+    }
+
+    public static Gson getGson() {
+        return NWTWEngine.getPlugin().getGson();
+    }
+
+    public static TranslateManager getTranslateManager() {
+        return NWTWEngine.getPlugin().getTranslateManager();
+    }
+
+    public static CustomItemManager getCustomItemManager() {
+        return NWTWEngine.getPlugin().getCustomItemManager();
+    }
+}
